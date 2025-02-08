7e5b9949b0d4a6607f19d466daad5b2bd2d26c5a
Maaike Massarotto <domyna@live.nl>
2025-02-08 10:04:39 +0100

    bestand helper.py toegevoegd

---
+++ helper.py
@@ -0,0 +1,7 @@
+tekst = "header"
+lengte = len(tekst) + 4
+print() # empty line
+print(lengte * "*") # print 4 stars
+print(f"* {tekst} *") # print tekst
+print(lengte * "*") # print 4 stars
+print() # empty line
